341fd005d75e3e1705531643c193e05a49e65520
Lakshme Srada Srinivas <lsrinivas@scu.edu>
2025-04-20 14:39:31 -0700

    data converter

---
+++ flipkart/data_converter.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+#data converter - convert csv file to document format
+def dataconverter():
+    product_data = pd.read_csv("data/flipkart_product_review.csv")
+    
+    data = product_datapp[["product_title","review"]]
+    
+    product_list = [] 
+
+    #iterate over the rows of the df
+    for index, row in df.iterrows():
+        object = {
+            'product_name': row['product_title'], 
+            'review': row['review']
+        }
+
+        ## append to product list
+        product_list.append(object)
+        
+    #converting to document format
+    docs = []
+
+    for object in product_list: 
+        metadata = {'product_name' : object['product_name']}
+        page_content = object['review']
+
+        doc = Document(page_content = page_content, metadata = metadata)
+
+        docs.append(doc)
+        
+    return Docs
+        
